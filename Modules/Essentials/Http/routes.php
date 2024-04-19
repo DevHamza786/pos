ea134bf4f2115042d5063d97d224c4def2273044
DevHamza786 <muhammahhamzamehmood@gmail.com>
2024-04-19 15:20:06 +0500

    hrm portal link updated

--- Modules/Essentials/Http/routes.php
+++ Modules/Essentials/Http/routes.php
@@ -44,49 +44,47 @@ Route::group(['middleware' => ['web', 'authh', 'auth', 'SetSessionData', 'langua
     });
 
     Route::group(['prefix' => 'hrm'], function () {
-        Route::get('/https://hotel.magnifystudio.com.pk/public/login');
+        Route::get('/dashboard', 'DashboardController@hrmDashboard');
+        Route::resource('/leave-type', 'EssentialsLeaveTypeController');
+        Route::resource('/leave', 'EssentialsLeaveController');
+        Route::post('/change-status', 'EssentialsLeaveController@changeStatus');
+        Route::get('/leave/activity/{id}', 'EssentialsLeaveController@activity');
+        Route::get('/user-leave-summary', 'EssentialsLeaveController@getUserLeaveSummary');
 
-        // Route::get('/dashboard', 'DashboardController@hrmDashboard');
-        // Route::resource('/leave-type', 'EssentialsLeaveTypeController');
-        // Route::resource('/leave', 'EssentialsLeaveController');
-        // Route::post('/change-status', 'EssentialsLeaveController@changeStatus');
-        // Route::get('/leave/activity/{id}', 'EssentialsLeaveController@activity');
-        // Route::get('/user-leave-summary', 'EssentialsLeaveController@getUserLeaveSummary');
+        Route::get('/settings', 'EssentialsSettingsController@edit');
+        Route::post('/settings', 'EssentialsSettingsController@update');
 
-        // Route::get('/settings', 'EssentialsSettingsController@edit');
-        // Route::post('/settings', 'EssentialsSettingsController@update');
+        Route::post('/import-attendance', 'AttendanceController@importAttendance');
+        Route::resource('/attendance', 'AttendanceController');
+        Route::post('/clock-in-clock-out', 'AttendanceController@clockInClockOut');
 
-        // Route::post('/import-attendance', 'AttendanceController@importAttendance');
-        // Route::resource('/attendance', 'AttendanceController');
-        // Route::post('/clock-in-clock-out', 'AttendanceController@clockInClockOut');
+        Route::post('/validate-clock-in-clock-out', 'AttendanceController@validateClockInClockOut');
 
-        // Route::post('/validate-clock-in-clock-out', 'AttendanceController@validateClockInClockOut');
+        Route::get('/get-attendance-by-shift', 'AttendanceController@getAttendanceByShift');
+        Route::get('/get-attendance-by-date', 'AttendanceController@getAttendanceByDate');
+        Route::get('/get-attendance-row/{user_id}', 'AttendanceController@getAttendanceRow');
 
-        // Route::get('/get-attendance-by-shift', 'AttendanceController@getAttendanceByShift');
-        // Route::get('/get-attendance-by-date', 'AttendanceController@getAttendanceByDate');
-        // Route::get('/get-attendance-row/{user_id}', 'AttendanceController@getAttendanceRow');
+        Route::get(
+            '/user-attendance-summary',
+            'AttendanceController@getUserAttendanceSummary'
+        );
 
-        // Route::get(
-        //     '/user-attendance-summary',
-        //     'AttendanceController@getUserAttendanceSummary'
-        // );
+        Route::get('/location-employees', 'PayrollController@getEmployeesBasedOnLocation');
+        Route::get('/my-payrolls', 'PayrollController@getMyPayrolls');
+        Route::get('/get-allowance-deduction-row', 'PayrollController@getAllowanceAndDeductionRow');
+        Route::get('/payroll-group-datatable', 'PayrollController@payrollGroupDatatable');
+        Route::get('/view/{id}/payroll-group', 'PayrollController@viewPayrollGroup');
+        Route::get('/edit/{id}/payroll-group', 'PayrollController@getEditPayrollGroup');
+        Route::post('/update-payroll-group', 'PayrollController@getUpdatePayrollGroup');
+        Route::get('/payroll-group/{id}/add-payment', 'PayrollController@addPayment');
+        Route::post('/post-payment-payroll-group', 'PayrollController@postAddPayment');
+        Route::resource('/payroll', 'PayrollController');
+        Route::resource('/holiday', 'EssentialsHolidayController');
 
-        // Route::get('/location-employees', 'PayrollController@getEmployeesBasedOnLocation');
-        // Route::get('/my-payrolls', 'PayrollController@getMyPayrolls');
-        // Route::get('/get-allowance-deduction-row', 'PayrollController@getAllowanceAndDeductionRow');
-        // Route::get('/payroll-group-datatable', 'PayrollController@payrollGroupDatatable');
-        // Route::get('/view/{id}/payroll-group', 'PayrollController@viewPayrollGroup');
-        // Route::get('/edit/{id}/payroll-group', 'PayrollController@getEditPayrollGroup');
-        // Route::post('/update-payroll-group', 'PayrollController@getUpdatePayrollGroup');
-        // Route::get('/payroll-group/{id}/add-payment', 'PayrollController@addPayment');
-        // Route::post('/post-payment-payroll-group', 'PayrollController@postAddPayment');
-        // Route::resource('/payroll', 'PayrollController');
-        // Route::resource('/holiday', 'EssentialsHolidayController');
-
-        // Route::get('/shift/assign-users/{shift_id}', 'ShiftController@getAssignUsers');
-        // Route::post('/shift/assign-users', 'ShiftController@postAssignUsers');
-        // Route::resource('/shift', 'ShiftController');
-        // Route::get('/sales-target', 'SalesTargetController@index');
+        Route::get('/shift/assign-users/{shift_id}', 'ShiftController@getAssignUsers');
+        Route::post('/shift/assign-users', 'ShiftController@postAssignUsers');
+        Route::resource('/shift', 'ShiftController');
+        Route::get('/sales-target', 'SalesTargetController@index');
         // Route::get('/set-sales-target/{id}', 'SalesTargetController@setSalesTarget');
         // Route::post('/save-sales-target', 'SalesTargetController@saveSalesTarget');
     });
